19b4892a5659e313ccce37fb1ac7adbdb2204f87
GmMamunH <gmmamun3004@gmail.com>
2025-01-31 12:37:45 +0600

    add(image): add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-
+import Image from "next/image";
+import gmh from "@/public/gmh-avator.jpg";
 
 export default function HomePage() {
   return (
     <>
       <div>
-        <h1 className="text-2xl text-center text-red-500 p-4">
-          Hello Next JS
-        </h1>
-       
+        <h1 className="text-2xl text-center text-red-500 p-4">Hello Next JS</h1>
+        <Image
+          src={gmh}
+          alt="gmh"
+          width={500} // Adjust width as needed
+          height={500} // Adjust height as needed
+        />
       </div>
     </>
   );
