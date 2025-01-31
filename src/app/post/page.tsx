0d778a9485503233053916dc16f746c9995b56cc
GmMamunH <gmmamun3004@gmail.com>
2025-01-31 22:45:53 +0600

    add(post): add type in post page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,21 +1,26 @@
-
 import Link from "next/link";
 
-const getPosts = async () => {
+type PostType = {
+  id: number; // ID সংখ্যা, তাই number ব্যবহার করা হলো
+  title: string;
+};
+
+const getPosts = async (): Promise<PostType[]> => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   return res.json();
 };
 
-const PostPage = async() => {
-    const posts = await getPosts();
+const PostPage = async () => {
+  const posts: PostType[] = await getPosts();
+
   return (
     <div>
       <h1 className="text-center text-2xl font-bold py-2">
         Server-Side Rendering
       </h1>
-      
+
       <ul className="grid grid-cols-2 md:grid-cols-4 gap-3 py-6">
-        {posts.slice(0, 12).map((post: any) => (
+        {posts.slice(0, 12).map((post: PostType) => (
           <li key={post.id}>
             <div className="shadow-lg p-4 border rounded-lg">
               <Link href={`/post/${post.id}`}>
